42da2228e97d349beec020b4c6ada311adb74fb6
Aleksandr Harisov <bwb-a.harisov@fondespierre.com>
2023-08-21 12:21:34 +0200

    SW-24: insert person data to person page

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,27 @@ const baseUrl = "https://swapi.dev/api/";
 
 // get person's data from SWAPI
 const getPersonData = () => {
+    // url to get person data
     const peopleUrl = "people/1/";
 
+    // find html elements for person
+    const personNameElement = document.querySelector("#person-card h2");
+    const personHeightElement = document.querySelector("#person-card .height span");
+    const personPlanetElement = document.querySelector("#person-card .planet");
+
+    // ajax request to get person data
     $.ajax({
-        url: baseUrl + peopleUrl,
-        success: function() {
-            console.log('ready');
-        }
+        url: baseUrl + peopleUrl
     }).done(function( data ) {
         console.log( data );
+
+        const personName = data.name;
+        const personHeight = data.height;
+        const personPlanetUrl = data.homeworld;
+
+        personNameElement.innerHTML = personName;
+        personHeightElement.innerHTML = personHeight;
+        personPlanetElement.setAttribute("data-num", personPlanetUrl);
     });
 }
 
