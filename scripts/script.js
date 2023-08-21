7d28ce23e29bca53196f66e4e1eec344c72a2ae1
Aleksandr Harisov <bwb-a.harisov@fondespierre.com>
2023-08-21 15:18:26 +0200

    SW-26: get and insert planet data

--- scripts/script.js
+++ scripts/script.js
@@ -4,39 +4,14 @@ const baseUrl = "https://swapi.dev/api/";
 // counter for getting people
 let cnt = 1;
 
-// get person's data from SWAPI
-const getPersonData = () => {
-    // url to get person data
-    const peopleUrl = `people/${cnt}/`;
-
-    // find html elements for person
-    const personNameElement = document.querySelector("#person-card h2");
-    const personHeightElement = document.querySelector("#person-card .height span");
-    const personPlanetElement = document.querySelector("#person-card .planet");
-
+// add previous btn 
+const addPrevBtn = () => {
     // previous btn
     const btnPrevIcon = `
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
         </svg>
     `;
-
-    // ajax request to get person data
-    $.ajax({
-        url: baseUrl + peopleUrl
-    }).done(function( data ) {
-        console.log( data );
-
-        // save person's data
-        const personName = data.name;
-        const personHeight = data.height;
-        const personPlanetUrl = data.homeworld;
-
-        // add person's data to html
-        personNameElement.innerHTML = personName;
-        personHeightElement.innerHTML = personHeight;
-        personPlanetElement.setAttribute("data-num", personPlanetUrl);
-    });
 
     // add previous btn after moving to 2nd person
     const btnPrevDiv = document.createElement("div");
@@ -54,10 +29,40 @@ const getPersonData = () => {
     } 
     
     // remove previous button if counter is 1
-    if (cnt == 1) {    
+    if (cnt == 1 && btnPrev) {    
         btnPrev.remove();
         arrowsContainer.classList.remove("has-elements");
     }
+}
+
+// get person's data from SWAPI
+const getPersonData = () => {
+    // url to get person data
+    const peopleUrl = `people/${cnt}/`;
+
+    // find html elements for person
+    const personNameElement = document.querySelector("#person-card h2");
+    const personHeightElement = document.querySelector("#person-card .height span");
+    const personPlanetElement = document.querySelector("#person-card .planet");
+
+    // ajax request to get person data
+    $.ajax({
+        url: baseUrl + peopleUrl,
+        success: data => {
+            console.log( data );
+    
+            // save person's data
+            const personName = data.name;
+            const personHeight = data.height;
+    
+            // add person's data to html
+            personNameElement.innerHTML = personName;
+            personHeightElement.innerHTML = personHeight;
+            personPlanetElement.setAttribute("href", `/planet.html?num=${cnt}`);
+        }
+    });
+
+    addPrevBtn();
 }
 
 // get next person
@@ -79,4 +84,40 @@ const getPrevPerson = () => {
     console.log(cnt);
 }
 
-getPersonData();
+// get person's data from SWAPI
+const getPlanetData = () => {
+    // get url param 
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let id;
+    if (urlParams.has('num')) {
+        id = urlParams.get('num');    
+    } else {
+        id = 1;
+    }
+
+    console.log(urlParams.get('num'));
+
+    // url to get planet data
+    const url = `planets/${id}/`;
+
+    // find html elements for planet
+    const planetNameElement = document.querySelector("#planet-card h2");
+    const planetTerrainElement = document.querySelector("#planet-card .terrain");
+
+    // ajax request to get planet data
+    $.ajax({
+        url: baseUrl + url,
+        success: data => {
+            console.log( data );
+
+            // save person's data
+            const planetName = data.name;
+            const planetTerrain = data.terrain;
+
+            // add person's data to html
+            planetNameElement.innerHTML = planetName;
+            planetTerrainElement.innerHTML = planetTerrain;
+        }
+    });
+}
